4486295fa85e57065d221760e8b7ab9f1cb63473
332938647@qq.com <332938647@qq.com>
2016-12-01 20:06:03 +0800

    scandate 备份 writeJson修改写入方式

--- task/writeJsonFile.py
+++ task/writeJsonFile.py
@@ -9,27 +9,21 @@ import config
 def writeFile(inputfile,data):
     if os.path.isfile(inputfile):
 
+        print inputfile
+        print data
         ##方式2 json读取，转成dict对象(类似json对象)，用键值对来访问
-        fileIn = open(inputfile , 'rw')
-
+        fileIn = open(inputfile, 'w+')
         outStr = json.dumps(data, ensure_ascii=False)   # 处理完之后重新转为Json格式
-
         fileIn.write(outStr)
         fileIn.close()
-
         return{'errcode':0,"data":''}
 
     else:
         confg.creatRootDirectories()
-
-
-        fileIn = open(inputfile, 'rw')
-
+        fileIn = open(inputfile, 'w+')
         outStr = json.dumps(data, ensure_ascii=False)  # 处理完之后重新转为Json格式
-
         fileIn.write(outStr)
         fileIn.close()
-
         return {'errcode': 0, "data": ''}
 
 
